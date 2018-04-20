a3ce7d2d9bed87027a4b9281d00cff525ec3b69f
33jain33confido <33jain33confido@gmail.com>
2018-04-20 16:44:39 +0530

    Upload project to github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular proggresive app';
+  title = 'Angular proggresive app1';
 }
